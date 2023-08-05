20e5aefbe3d9f55bcf84a692263b61e638b55b75
Tatiana Smolina <smolina-ta97@yandex.ru>
2023-08-05 23:31:48 +1000

    feat: подключить прелоадер к сохраненным фильмам

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -5,6 +5,7 @@ import NavLogin from '../NavLogin/NavLogin';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
+import Preloader from '../Preloader/Preloader';
 
 function SavedMovies({savedMovies, handleSearchSavedMovie, isServerError, isNotFound, isLoading, isMoviesPage}) {
   return (
@@ -15,17 +16,23 @@ function SavedMovies({savedMovies, handleSearchSavedMovie, isServerError, isNotF
       <main className="movies-save">
         <div className="movies-save__container">
           <SearchForm handleSearchSavedMovie={handleSearchSavedMovie} isMoviesPage={isMoviesPage} />
-          <p className={`${isServerError
-            ? 'movies-save__message' 
-            : 'movies-save__message_type_disabled'}`}>
-              Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз.
-          </p>
-          <p className={`${isNotFound
-            ? 'movies-save__message' 
-            : 'movies-save__message_type_disabled'}`}>
-              По вашему запросу ничего не найденно.
-          </p>
-          <MoviesCardList savedMovies={savedMovies} isMoviesPage={isMoviesPage} />
+          {isLoading ?(
+            <Preloader />)
+          : (
+            <>
+              <p className={`${isServerError
+                ? 'movies-save__message' 
+                : 'movies-save__message_type_disabled'}`}>
+                  Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз.
+              </p>
+              <p className={`${isNotFound
+                ? 'movies-save__message' 
+                : 'movies-save__message_type_disabled'}`}>
+                  По вашему запросу ничего не найденно.
+              </p>
+              <MoviesCardList savedMovies={savedMovies} isMoviesPage={isMoviesPage} />
+            </>
+          )}
         </div>
       </main>
       <Footer />
